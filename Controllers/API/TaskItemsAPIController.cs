11e776f63db675fcbe85984b66e4c2639a323ea6
dongp2505 <dongp2505@gmail.com>
2025-12-07 15:39:50 -0500

    Added Identity only for admin can use the API. And scaffolder API with Entity Framework for employee, project, and task item

--- Controllers/API/TaskItemsAPIController.cs
+++ Controllers/API/TaskItemsAPIController.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Capstone_Project_PROG36944.Data;
+using Capstone_Project_PROG36944.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Capstone_Project_PROG36944.Data;
-using Capstone_Project_PROG36944.Models;
 
 namespace Capstone_Project_PROG36944.Controllers.API
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "Admin")]
     public class TaskItemsAPIController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
