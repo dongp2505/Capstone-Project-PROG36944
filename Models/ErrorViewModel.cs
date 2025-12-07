7e490b0a7b24f7bf72383be02e1dc458e0ec2e11
dongp2505 <dongp2505@gmail.com>
2025-12-07 15:11:45 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Capstone_Project_PROG36944.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
